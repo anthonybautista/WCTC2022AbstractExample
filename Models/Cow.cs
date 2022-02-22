bf46685cf9a89df474b80f435887cd8d84ae5829
Anthony Bautista <anthonya.bautista@gmail.com>
2022-02-21 20:40:45 -0600

    Add Cow

---
+++ Models/Cow.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AbstractExample.Models
+{
+    public class Cow : Animal
+    {
+        public Cow(string name)
+        {
+            Name = name;
+        }
+
+        public Cow()
+        {
+            Name = "Bessie";
+        }
+
+        public override void MakeNoise()
+        {
+            Console.WriteLine($"{Name} says mooooooooooo!");
+        }
+        
+        public override void Sleep()
+        {
+            Console.WriteLine("zZzZzZzZz...");
+        }
+    }
+}
